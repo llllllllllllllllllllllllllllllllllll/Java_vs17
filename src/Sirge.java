3df4a03ee83d54dec24ae6561783e18cd6185e81
aleksander <aleksander.promvalds@khk.ee>
2018-10-15 12:46:57 +0300

    Task 5 - creating Sirge object works.

--- src/Sirge.java
+++ src/Sirge.java
@@ -1,4 +1,18 @@
 public class Sirge {
+
+    Punkt endP;
+    Punkt startP;
+    double len;
+
     public Sirge(Punkt startPoint, Punkt endPoint){
+        this.startP = startPoint;
+        this.endP = endPoint;
+
+        // calculate length
+        this.len = Math.sqrt((startPoint.getXCoordinate() - endPoint.getXCoordinate()) * (startPoint.getXCoordinate() - endPoint.getXCoordinate()) + (startPoint.getYCoordinate() - endPoint.getYCoordinate()) * (startPoint.getYCoordinate() - endPoint.getYCoordinate()));
+    }
+
+    double getLength(){
+        return len;
     }
 }
--- src/Ul5.java
+++ src/Ul5.java
@@ -2,8 +2,13 @@ public class Ul5 {
     public static void main(String[] args){
 
         Punkt punkt1 = new Punkt(-1, 2);
+        Punkt punkt2 = new Punkt(-1, 3);
 
         System.out.println(punkt1.getInfo());
+
+        Sirge sirge1 = new Sirge(punkt1, punkt2);
+
+        System.out.println(sirge1.getLength());
 
     }
 }
